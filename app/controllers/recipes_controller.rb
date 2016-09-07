eb3a04d1f5acbbb16aae1bb452ed4f973d10a314
Md. Dinar <dinar.cse@gmail.com>
2016-09-07 11:11:30 +0600

    add user auth with devise

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,23 +1,29 @@
 class RecipesController < ApplicationController
-  before_action :find_recipe, only: [:show, :edit, :update, :destroy]
-  def index
-    @recipe = Recipe.all.order("created_at DESC")
-  end
-  def new
-    @recipe= Recipe.new
-  end
-  def create
-    @recipe= Recipe.new(recipe_params)
-    if @recipe.save
+	before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, except: [:index, :show]
+
+	def index
+		@recipe = Recipe.all.order("created_at DESC")
+	end
+
+	def show
+	end
+
+	def new
+		@recipe = current_user.recipes.build
+	end
+
+	def create
+		@recipe = current_user.recipes.build(recipe_params)
+
+		if @recipe.save
 			redirect_to @recipe, notice: "Successfully created new recipe"
 		else
 			render 'new'
-end
-  end
-  def show
-  end
+		end
+	end
 
-  def edit
+	def edit
 	end
 
 	def update
@@ -33,12 +39,13 @@ end
 		redirect_to root_path, notice: "Successfully deleted recipe"
 	end
 
-  private
-    def recipe_params
-      params.require(:recipe).permit(:title, :description, :image, ingredients_attributes: [:id, :name, :_destroy],
-        directions_attributes: [:id, :step, :_destroy])
-    end
-    def find_recipe
-      @recipe = Recipe.find(params[:id])
-    end
+	private
+
+	def recipe_params
+		params.require(:recipe).permit(:title, :description, :image, ingredients_attributes: [:id, :name, :_destroy], directions_attributes: [:id, :step, :_destroy])
+	end
+
+	def find_recipe
+		@recipe = Recipe.find(params[:id])
+	end
 end
